3ff9d77e2e575ffd731788a8f078bb8587c95ccc
k0j1 <k0j123t@gmail.com>
2021-07-11 07:04:49 +0900

    Initial commit

---
+++ app/src/main/java/com/takaharabooks/app/news/umamusume/pref/PreferenceDataFragment.java
@@ -0,0 +1,14 @@
+package com.takaharabooks.app.news.umamusume.pref;
+
+import android.os.Bundle;
+
+import androidx.preference.PreferenceFragmentCompat;
+
+public class PreferenceDataFragment extends PreferenceFragmentCompat
+{
+        @Override
+        public void onCreatePreferences(Bundle savedInstanceState, String rootKey)
+        {
+                //setPreferencesFromResource(R.layout.fragment_setting, rootKey);
+        }
+}
